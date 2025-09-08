16fd27eb046b866151aed0fbb0a5cc9a27805070
Elijah ombogo <elijahombogo2004@gmail.com>
2025-09-08 23:47:04 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by ʜᴜɴᴛᴇʀ xᴍᴅ 💜`
+      `> © Uploaded by elijah🔰`
     );
 
   } catch (error) {

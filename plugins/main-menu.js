f23f1e76cda6aae04c9c8db649def27dce4d875f
Elijah ombogo <elijahombogo2004@gmail.com>
2025-09-13 15:15:28 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -362,8 +362,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://raw.githubusercontent.com/ELIJAH-Ombogo/elijah/main/53aa1d%20(1).mp3' },
-            mimetype: 'audio/mp3',
+            audio: { url: 'https://raw.githubusercontent.com/ELIJAH-Ombogo/elijah/main/VID-20250913-WA0142.mp4' },
+            mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
         

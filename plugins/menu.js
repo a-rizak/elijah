1d4e8e88ec052b223c35a9e70ca154c8478821dc
Elijah ombogo <elijahombogo2004@gmail.com>
2025-09-13 14:32:51 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -66,7 +66,7 @@ cmd({
 
         // Send menu audio only once
         await conn.sendMessage(from, {
-            audio: { url: '' },
+            audio: { url: 'https://raw.githubusercontent.com/ELIJAH-Ombogo/elijah/main/53aa1d%20(1).mp3' },
             mimetype: 'audio/mp3',
             ptt: true,       
         }, { quoted: mek });
